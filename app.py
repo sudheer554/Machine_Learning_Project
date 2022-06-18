278ae09648112a6b0986f54c20c50b97fc3cb3b6
sudheer554 <sudheer.ece2008@gmail.com>
2022-06-19 00:02:22 +0530

    docker cd

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machine Learning Project"
+    return "CI / CD pipe line for Starting Machine Learning Project"
 
 
 if __name__=="__main__":
