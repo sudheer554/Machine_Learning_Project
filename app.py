797155fd806c6e4979ed6e2fb4e61ced8c3082a7
sudheer554 <sudheer.ece2008@gmail.com>
2022-06-19 07:17:19 +0530

    input fun

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "CI / CD pipe line for Starting Machine Learning Project"
+    return "s = int(input("type number"))"
 
 
 if __name__=="__main__":
